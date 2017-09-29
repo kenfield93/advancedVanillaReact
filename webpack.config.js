781fc9903c8fee54de961d1f1f0c6e0d2eaa8384
Kyle Enfield <kenfield93@gmail.com>
2017-09-29 12:53:23 -0700

    added tests, and set NODE_PATH and webpack.config to look under lib for code. Also refactored our mock api to a seperate package (state_api)

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 //import path from 'path';
 const path = require('path');
 
+// the resolve{modules:[]} setting is just to tell webpack were to look for files, not to tell node
 const config = {
-    entry: ['babel-polyfill', "./lib/components/index.js"],
+    resolve: {
+        modules: [
+            path.resolve('./lib'),
+            path.resolve('./node_modules')
+        ]
+    },
+    entry: ['babel-polyfill', './lib/renderers/domRenderer.js'],
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
-    module:{
+    module: {
         rules: [
-            {test: /\.(js|jsx)$/, exclude: '/node_modules', use: 'babel-loader'}
+            {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
         ]
     }
-
 };
 
 module.exports = config;
-//export default config;
